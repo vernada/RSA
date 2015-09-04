5e35e7311bb5d9305dae9147fe2544ceb219bdbb
Emir Enler <insolid@outlook.com>
2015-09-04 17:43:12 +0200

    Base64Code class references are deleted from the Main class to assure integrity.

--- src/com/insolid/rsaencr/Main.java
+++ src/com/insolid/rsaencr/Main.java
@@ -13,16 +13,13 @@ public class Main {
     *@param args {bit length of RSA key, message}
     */
     public static void main(String[] args) {
-        int length = args.length;
-        if(length != 2) return;
+        if(args.length != 2) return;
 
         RSA rsa = RSA.getInstance(Integer.parseInt(args[0]));
         String message = args[1];
-        BigInteger encrypted = rsa.encrypt(message);
-        String encodedMessage = Base64Code.encode(encrypted);
-        BigInteger decodedMessage = Base64Code.decode(encodedMessage);
-        String decrypted = rsa.decrypt(decodedMessage);
+        String encryptedMessage = rsa.encrypt(message);
+        String decryptedMessage = rsa.decrypt(encryptedMessage);
 
-        System.out.println(message + "\n" + encrypted + "\n" + encodedMessage + "\n" + decodedMessage + "\n" + decrypted);
+        System.out.println(message + "\n" + encryptedMessage + "\n" + decryptedMessage);
     }
 }
